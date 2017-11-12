c55ac5e626b951bf7ea0f11f81245f6dbe4a865f
jason bronson <jasonbronson@gmail.com>
2017-11-12 10:20:03 -0800

    add in 1st version

---
+++ myenv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+username = os.environ.get("username")
+password = os.environ.get("password")
+httpid = os.environ.get("httpid");
+ip = os.environ.get("ip");

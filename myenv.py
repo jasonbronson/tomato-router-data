870afb38f1a0a85350ace36cd1d69ac247ea66fb
jason bronson <jasonbronson@gmail.com>
2017-12-12 19:03:08 -0800

    bug fix for env

--- myenv.py
+++ myenv.py
@@ -7,8 +7,12 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-username = os.environ.get("username")
-password = os.environ.get("password")
-httpid = os.environ.get("httpid")
-ip = os.environ.get("ip")
-redishost = os.environ.get("redishost")
+
+MYSQL_USERNAME=os.environ.get("MYSQL_USERNAME")
+MYSQL_PASSWORD=os.environ.get("MYSQL_PASSWORD")
+MYSQL_IP=os.environ.get("MYSQL_IP")
+ROUTER_USERNAME=os.environ.get("ROUTER_USERNAME")
+ROUTER_PASSWORD=os.environ.get("ROUTER_PASSWORD")
+ROUTER_IP=os.environ.get("ROUTER_IP")
+HTTP_ID=os.environ.get("HTTP_ID")
+REDIS_HOST = os.environ.get("REDIS_HOST")
